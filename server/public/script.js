d76d1e51bba51a41392e4a691cd45a5833972d8f
doi lam <doitunglam1@gmail.com>
2024-06-27 14:13:15 +0700

    update

--- server/public/script.js
+++ server/public/script.js
@@ -1,194 +1,213 @@
 const socket = io();
 
-const waterHeightCtx = document.getElementById('waterHeightChart').getContext('2d');
-const temperatureCtx = document.getElementById('temperatureChart').getContext('2d');
-const rainCtx = document.getElementById('rainChart').getContext('2d');
-const damLevelCtx = document.getElementById('damLevelChart').getContext('2d');
+const waterHeightCtx = document
+  .getElementById("waterHeightChart")
+  .getContext("2d");
+const temperatureCtx = document
+  .getElementById("temperatureChart")
+  .getContext("2d");
+const rainCtx = document.getElementById("rainChart").getContext("2d");
+const damLevelCtx = document.getElementById("damLevelChart").getContext("2d");
 
 const waterHeightChart = new Chart(waterHeightCtx, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Water Height (cm)',
-            data: [],
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-            fill: false
-        }]
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Water Height (cm)",
+        data: [],
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    responsive: false, // Ensures the chart resizes with the window
+    maintainAspectRatio: false, // Allow the chart to fill the canvas dimensions
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "minute",
+        },
+        title: {
+          display: true,
+          text: "Time",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Water Height (cm)",
+        },
+      },
     },
-    options: {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'minute'
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'Water Height (cm)'
-                }
-            }
-        }
-    }
+  },
 });
 
 const temperatureChart = new Chart(temperatureCtx, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Temperature',
-            data: [],
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-            fill: false
-        }]
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Temperature",
+        data: [],
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    responsive: false, // Ensures the chart resizes with the window
+    maintainAspectRatio: false, // Allow the chart to fill the canvas dimensions
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "minute",
+        },
+        title: {
+          display: true,
+          text: "Time",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Temperature",
+        },
+      },
     },
-    options: {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'minute'
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'Temperature'
-                }
-            }
-        }
-    }
+  },
 });
 
-
 const rainChart = new Chart(rainCtx, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Rain (cm)',
-            data: [],
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-            fill: false
-        }]
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Rain (cm)",
+        data: [],
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    responsive: false, // Ensures the chart resizes with the window
+    maintainAspectRatio: false, // Allow the chart to fill the canvas dimensions
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "minute",
+        },
+        title: {
+          display: true,
+          text: "Time",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Rain (cm)",
+        },
+      },
     },
-    options: {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'minute'
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'Rain (cm)'
-                }
-            }
-        }
-    }
+  },
 });
 
 const damLevelChart = new Chart(damLevelCtx, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Dam Level (cm)',
-            data: [],
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            fill: false,
-            stepped: true
-        }]
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Dam Level (cm)",
+        data: [],
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+        fill: false,
+        stepped: true,
+      },
+    ],
+  },
+  options: {
+    responsive: false, // Ensures the chart resizes with the window
+    maintainAspectRatio: false, // Allow the chart to fill the canvas dimensions
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "minute",
+        },
+        title: {
+          display: true,
+          text: "Time",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Dam Level",
+        },
+        ticks: {
+          stepSize: 20,
+          callback: function (value) {
+            return value + "cm";
+          },
+        },
+      },
     },
-    options: {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'minute'
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'Dam Level'
-                },
-                ticks: {
-                    stepSize: 20,
-                    callback: function(value) {
-                        return value + 'cm';
-                    }
-                }
-            }
-        }
-    }
+  },
 });
 
-socket.on('waterHeight', (data) => {
-    const now = new Date(data.timestamp);
-    if (waterHeightChart.data.labels.length >= 20) {
-        waterHeightChart.data.labels.shift();
-        waterHeightChart.data.datasets[0].data.shift();
-    }
-    waterHeightChart.data.labels.push(now);
-    waterHeightChart.data.datasets[0].data.push(data.height);
-    waterHeightChart.update();
+socket.on("waterHeight", (data) => {
+  const now = new Date(data.timestamp);
+  if (waterHeightChart.data.labels.length >= 20) {
+    waterHeightChart.data.labels.shift();
+    waterHeightChart.data.datasets[0].data.shift();
+  }
+  waterHeightChart.data.labels.push(now);
+  waterHeightChart.data.datasets[0].data.push(data.height);
+  waterHeightChart.update();
 });
 
-socket.on('temperature', (data) => {
-    const now = new Date(data.timestamp);
-    if (temperatureChart.data.labels.length >= 20) {
-        temperatureChart.data.labels.shift();
-        temperatureChart.data.datasets[0].data.shift();
-    }
-    temperatureChart.data.labels.push(now);
-    temperatureChart.data.datasets[0].data.push(data.temperature);
-    temperatureChart.update();
+socket.on("temperature", (data) => {
+  const now = new Date(data.timestamp);
+  if (temperatureChart.data.labels.length >= 20) {
+    temperatureChart.data.labels.shift();
+    temperatureChart.data.datasets[0].data.shift();
+  }
+  temperatureChart.data.labels.push(now);
+  temperatureChart.data.datasets[0].data.push(data.temperature);
+  temperatureChart.update();
 });
 
-socket.on('rain', (data) => {
-    const now = new Date(data.timestamp);
-    if (rainChart.data.labels.length >= 20) {
-        rainChart.data.labels.shift();
-        rainChart.data.datasets[0].data.shift();
-    }
-    rainChart.data.labels.push(now);
-    rainChart.data.datasets[0].data.push(data.rain);
-    rainChart.update();
+socket.on("rain", (data) => {
+  const now = new Date(data.timestamp);
+  if (rainChart.data.labels.length >= 20) {
+    rainChart.data.labels.shift();
+    rainChart.data.datasets[0].data.shift();
+  }
+  rainChart.data.labels.push(now);
+  rainChart.data.datasets[0].data.push(data.rain);
+  rainChart.update();
 });
 
-socket.on('damStatus', (data) => {
-    const now = new Date();
-    if (damLevelChart.data.labels.length >= 20) {
-        damLevelChart.data.labels.shift();
-        damLevelChart.data.datasets[0].data.shift();
-    }
-    damLevelChart.data.labels.push(now);
-    damLevelChart.data.datasets[0].data.push(data.level);
-    damLevelChart.update();
+socket.on("damStatus", (data) => {
+  const now = new Date();
+  if (damLevelChart.data.labels.length >= 20) {
+    damLevelChart.data.labels.shift();
+    damLevelChart.data.datasets[0].data.shift();
+  }
+  damLevelChart.data.labels.push(now);
+  damLevelChart.data.datasets[0].data.push(data.level);
+  damLevelChart.update();
 });
